ccf686267d77da0ca90c3b6ca7c780ed06d68d7f
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-05-28 22:35:48 +0200

    comments

--- personalScripts/update-repoScripts.sh
+++ personalScripts/update-repoScripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MAINFOLDER="../.."
-SCRIPTSFOLDER="../repoScripts"
-SCRIPTS=("init-repository.sh" "build-repository.sh")
-FOLDERS=("SSVEntitySystem" "SSVLuaWrapper" "SSVUtils" "SSVUtilsJson" "SSVJsonCpp" "SSVSCollision" "SSVNewRogue" "SSVMenuSystem" "SSVStart")
+MAINFOLDER="../.." # This is "OHWorkspace/"
+SCRIPTSFOLDER="../repoScripts" # This is "OHWorkspace/VRDevUtils/repoScripts"
+SCRIPTS=("init-repository.sh" "build-repository.sh") # Consider gathering these with a loop?
+FOLDERS=("SSVEntitySystem" "SSVLuaWrapper" "SSVUtils" "SSVUtilsJson" "SSVJsonCpp" "SSVSCollision" "SSVNewRogue" "SSVMenuSystem" "SSVStart") # Consider gathering these with a loop?
 
 for script in ${SCRIPTS[*]}; do
 	for dir in ${FOLDERS[*]}; do if [ -f ${MAINFOLDER}/${dir}/${script} ]; then cp ${SCRIPTSFOLDER}/${script} ${MAINFOLDER}/${dir}/; fi; done
