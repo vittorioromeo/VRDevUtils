8071921d7a1d31dda7caa49a435efe43a08191ee
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-21 10:06:18 +0200

    updated build-repository

--- repoScripts/build-repository.sh
+++ repoScripts/build-repository.sh
@@ -5,41 +5,62 @@
 PROJECTNAME=${PWD##*/} # Project to build (current directory name)
 BUILDTYPE="RELEASE" # Passed to CMake (CMAKE_BUILD_TYPE)
 BUILDSHARED="TRUE" # Passed to CMake (LIBNAME_BUILD_SHARED_LIB)
+MAKEJOBS="4" # make -j...
 
 LIBS=() # List of extlibs to build (gathered from ./extlibs/*)
 for dir in ./extlibs/*; do LIBS+=(${dir##*/}); done	# Fill LIBS
 
+function warn() 
+{
+	echo "Error occured in: `pwd`"
+	echo "Error was: "$@
+}
+
+function die() 
+{
+	status=$1; shift
+	warn "$@" >&2
+	exit $status
+}
+
+
 # Builds a lib, with name $1 - calls CMake, make -j and make install -j
-function buildLib 
+function buildLib
 {
 	local LIBNAME="$1"
 	local ULIBNAME=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 
-	echo "Building $ULIBNAME..." 
+	echo "Building $ULIBNAME..."
   	cd $LIBNAME # Enter lib main directory (where CMakeLists.txt is)
   	rm CMakeCache.txt # Remove CMakeCache.txt, in case an earlier (accidental) build was made in the main directory1
   	mkdir build; cd build # Create and move to the build directory
   	rm CMakeCache.txt # If the library was previously built, remove CMakeCache.txt
 
-  	# Run CMake, make and make install
-	cmake ../ -D"$ULIBNAME"_BUILD_SHARED_LIB=$BUILDSHARED -DCMAKE_BUILD_TYPE=$BUILDTYPE
-	make -j; make install -j
+	# Run CMake, make and make install
+	cmake ../ -D"$ULIBNAME"_BUILD_SHARED_LIB=$BUILDSHARED -DCMAKE_BUILD_TYPE=$BUILDTYPE || \
+		die 1 "cmake failed"
+
+	make -j$MAKEJOBS || \
+		die 1 "make failed"
+
+	make install -j$MAKEJOBS || \
+		die 1 "make install failed"
 
 	cd ../.. # Go back to extlibs directory
 	echo "Finished building $ULIBNAME..."
-}  
+}
 
 cd extlibs  # Start building... Enter extlibs, and build extlibs
 for LIB in ${LIBS[*]}; do buildLib $LIB; done
 cd .. # Now we are in the main folder
-echo "Building $PROJECTNAME..." 
+echo "Building $PROJECTNAME..."
 rm CMakeCache.txt # Remove CMakeCache.txt, in case an earlier (accidental) build was made in the main directory
 mkdir build; cd build # Create and move to the build directory
 rm CMakeCache.txt # If the library was previously built, remove CMakeCache.txt
 
 ## Run CMake, make and make install
 cmake ../ -DCMAKE_BUILD_TYPE=$BUILDTYPE
-make -j; make install -j
+make -j$MAKEJOBS; make install -j$MAKEJOBS
 
 cd ..
 echo "Finished building $PROJECTNAME..."
