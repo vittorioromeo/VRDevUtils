3e10b6eec122db762923cd9229290e8a469bdeee
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-21 11:28:32 +0200

    Improved cmake cache removal

--- personalScripts/cmakeCacheRemoveAll.sh
+++ personalScripts/cmakeCacheRemoveAll.sh
@@ -4,3 +4,6 @@ MAINFOLDER="../.." # This is "OHWorkspace/"
 
 find ${MAINFOLDER} -name "CMakeFiles" | xargs rm -R
 find ${MAINFOLDER} -name "CMakeCache.txt" | xargs rm
+find ${MAINFOLDER} -name "cmake_install.cmake" | xargs rm -R
+find ${MAINFOLDER} -name "build.ninja" | xargs rm -R
+find ${MAINFOLDER} -name "rules.ninja" | xargs rm -R
